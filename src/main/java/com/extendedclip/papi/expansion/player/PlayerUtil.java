ae96f5afdcf93507ede60d18ea6664f80f824a51
extendedclip <fairviewent@yahoo.com>
2018-03-12 10:32:07 -0400

    Added license header

--- src/main/java/com/extendedclip/papi/expansion/player/PlayerUtil.java
+++ src/main/java/com/extendedclip/papi/expansion/player/PlayerUtil.java
@@ -3,7 +3,26 @@ package com.extendedclip.papi.expansion.player;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
-
+/*
+*
+* Player-Expansion
+* Copyright (C) 2018 Ryan McCarthy
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*
+*/
 public class PlayerUtil {
 
 	public static final int ticksAtMidnight = 18000;
