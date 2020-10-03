2188684fbc723badabc6dc1b3400777b241a7555
PiggyPiglet <noreply@piggypiglet.me>
2020-10-03 14:58:56 +0800

    push for frosty idek what changes i've made

--- src/main/java/at/helpch/placeholderapi/expansion/player/PlayerExpansion.java
+++ src/main/java/at/helpch/placeholderapi/expansion/player/PlayerExpansion.java
@@ -12,8 +12,19 @@ import at.helpch.placeholderapi.api.expansion.Platform;
         platforms = {Platform.BUKKIT, Platform.SPONGE, Platform.NUKKIT}
 )
 public final class PlayerExpansion extends Expansion {
-    @Override
-    public void enable() {
-        
-    }
+//    @Override
+//    public void enable() {
+//        register();
+//    }
+//
+//    private void register() {
+//        player("name", player -> player.getNullable(PlayerKeys.NAME));
+//        player("uuid", player -> player.getNullable(PlayerKeys.UUID));
+//        player("has_played_before", player -> player.getNullable(PlayerKeys.HAS_PLAYED_BEFORE));
+//        player("is_whitelisted", player -> player.getNullable());
+//    }
+//
+//    private void player(@NotNull final String name, @NotNull final Function<Player<?>, Object> function) {
+//        registerPlaceholder(name, context -> function.apply(context.getPlayer()));
+//    }
 }
