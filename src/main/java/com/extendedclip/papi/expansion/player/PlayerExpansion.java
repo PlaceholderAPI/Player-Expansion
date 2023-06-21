dcfeec98edda6ced68d6507e749a40a390ffb1e9
iGabyTM <contactgabytm@gmail.com>
2023-06-21 20:51:34 +0300

    fix: move potion placeholders outside the `switch` block Because these placeholders requires an argument they need to be outside the switch block

--- src/main/java/com/extendedclip/papi/expansion/player/PlayerExpansion.java
+++ src/main/java/com/extendedclip/papi/expansion/player/PlayerExpansion.java
@@ -447,22 +447,24 @@ public final class PlayerExpansion extends PlaceholderExpansion implements Confi
                 return bool(p.isLeashed());
             case "is_inside_vehicle":
                 return bool(p.isInsideVehicle());
-            case "has_potion_effect": {
-                final PotionEffectType potion = PotionEffectType.getByName(identifier.replace("has_potion_effect_", ""));
-                return bool(potion != null && p.hasPotionEffect(potion));
-            }
-
-            case "potion_level": {
-                final PotionEffectType potion = PotionEffectType.getByName(identifier.replace("potion_level_", ""));
-
-                if (potion == null || !p.hasPotionEffect(potion)) {
-                    return "0";
-                }
-
-                //noinspection DataFlowIssue
-                return String.valueOf(p.getPotionEffect(potion).getAmplifier());
-            }
         }
+
+        if (identifier.startsWith("has_potion_effect_")) {
+            final PotionEffectType potion = PotionEffectType.getByName(identifier.replace("has_potion_effect_", ""));
+            return bool(potion != null && p.hasPotionEffect(potion));
+        }
+
+        if (identifier.startsWith("potion_level_")) {
+            final PotionEffectType potion = PotionEffectType.getByName(identifier.replace("potion_level_", ""));
+
+            if (potion == null || !p.hasPotionEffect(potion)) {
+                return "0";
+            }
+
+            //noinspection DataFlowIssue
+            return String.valueOf(p.getPotionEffect(potion).getAmplifier());
+        }
+
         // return null for unknown placeholders
         return null;
     }
